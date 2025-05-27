945cc8705ec5df4943e6ccacfd3daa7d4373e8bd
jpon0113 <jpongama@gamil.com>
2025-05-28 07:34:09 +0800

    feat: 配置配置winston log module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   const configServcie = app.get(ConfigService);
   const port = configServcie.get<number>('PORT', 3000);
