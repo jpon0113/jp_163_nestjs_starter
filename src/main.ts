42165fdd28c909c22ae9f97afe673dd9c218c833
jpon0113 <jpongama@gamil.com>
2025-05-28 07:57:51 +0800

    feat: 1. 設置api prefix 2. 錯誤異常捕獲 exceptionFilter 3. .env新增設定 4. 安裝request-ip

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { VersioningType } from '@nestjs/common';
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-
   const configServcie = app.get(ConfigService);
   const port = configServcie.get<number>('PORT', 3000);
+  const cors = configServcie.get('CORS', false);
+  const prefix = configServcie.get('PREFIX', '/api');
+  const version = configServcie.get<string>('VERSION', '1');
+  const errorFilterFlag = configServcie.get<string>('ERROR_FILTER');
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.setGlobalPrefix(prefix);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [version],
+  });
+
+  if (cors === 'true') {
+    app.enableCors();
+  }
+
+  if (errorFilterFlag === 'true') {
+    const httpAdapter = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+  }
+
   await app.listen(port);
 }
 bootstrap();
