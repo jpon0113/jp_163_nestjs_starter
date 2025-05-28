753bd30cc2a4ca4f3908d3479a28c584e9011a18
jpon0113 <jpongama@gamil.com>
2025-05-28 13:01:43 +0800

    feat: api version定義

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get()
+  // @Version('1')
   getHello(): string {
     return 'hello';
   }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { VersioningType } from '@nestjs/common';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { AppModule } from './app.module';
 
@@ -11,14 +11,19 @@ async function bootstrap() {
   const port = configServcie.get<number>('PORT', 3000);
   const cors = configServcie.get('CORS', false);
   const prefix = configServcie.get('PREFIX', '/api');
-  const version = configServcie.get<string>('VERSION', '1');
   const errorFilterFlag = configServcie.get<string>('ERROR_FILTER');
+  const versionStr = configServcie.get<string>('VERSION');
+  let version = [versionStr];
+  if (versionStr && versionStr.indexOf(',')) {
+    version = versionStr.split(',');
+  }
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.setGlobalPrefix(prefix);
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: [version],
+    defaultVersion:
+      typeof versionStr === 'undefined' ? VERSION_NEUTRAL : version,
   });
 
   if (cors === 'true') {
